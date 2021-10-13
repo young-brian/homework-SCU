304bd8e2fde791ea5324d85a0b96c2560ec0f7f3
young-brian <90754112+young-brian@users.noreply.github.com>
2021-10-13 14:48:05 +0800

    Add files via upload

--- 牛顿法求根号.cpp
+++ 牛顿法求根号.cpp
@@ -2,13 +2,13 @@
 #include<math.h>
 int main(){
 	//Çó¸ùºÅ
-	int x;
-	double y=1;
+	double x;
+	double y=1.0;
     printf("please input x:");
-	scanf("%d",&x);
+	scanf("%lf",&x);
 	while(y!=x/y){
 		y=(y+x/y)/2;
-		if(fabs(y-x/y)<0.0001){
+		if(fabs(y-x/y)<0.000001){
 			break;
 		} 
 	}
