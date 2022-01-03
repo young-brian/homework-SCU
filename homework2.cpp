45057980792904504e18b72b8bdeb049207a5b9a
young-brian <90754112+young-brian@users.noreply.github.com>
2022-01-03 17:30:15 +0800

    Add files via upload

---
+++ homework2.cpp
@@ -0,0 +1,37 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+int isprime(int x)
+{
+	for (int i = 2; i <= sqrt(x); i++)
+	{
+		if (x % i == 0)
+		{
+			return 0;
+		}
+	}
+	return 1;
+}
+
+//$p^ q + q ^ p$是一个素数，求$p$和$q$
+int main()
+{
+	int p=1, q=1;
+	int res = 0,cnt=0;
+	for (p = 1; p < 1000; p++)
+	{
+		for (q = 1; q < 100; q++)
+		{
+			if(isprime(pow(p, q) + pow(q, p)))
+			{
+				printf("%d %d\n", p, q);
+				cnt++;
+				if (cnt == 1000)
+					return 0;
+			}
+		}
+	}
+	return 0;
+}
